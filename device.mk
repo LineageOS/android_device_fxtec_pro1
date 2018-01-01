88461bc383d2ef4673ac74fdfc7585fba035d02b
Wei Wang <wvw@google.com>
2017-12-31 22:14:16 -0800

    pro1: power: switch to libperfmgr for powerhint
    Test: boot and do powerhint
    Bug: 62041945
    Change-Id: I7de1d2bf377fb46162171a084fca3413b1067d3b

--- device.mk
+++ device.mk
@@ -355,8 +355,10 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.2-service.pro1
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \

49728243fec020ccf3e2b247d4bf7f08b0b65bac
Sujin Panicker <spanic@codeaurora.org>
2019-07-16 18:26:34 +0530

    pro1: media: Fix rank of QC OMX components
    Rank OMX SW codecs higher than C2 codecs except for opus codec.
    
    Change-Id: Ided16f454fcb3b760076500e87759bcbb44f75ed

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -104,6 +104,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.nfc_nci=nqx.default
 
+# OMX
+# Rank OMX SW codecs lower than OMX HW codecs
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \

caa7d3c3d71885b0c432f910e86f4dd91cd368ab
Pranav Vashi <neobuddy89@gmail.com>
2019-09-19 00:33:20 +0530

    pro1: init: Convert to Android.bp
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I3b5f6e383af2d7ea481770ad5f17be76a184bbfa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,7 +177,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_fxtec_msm8998
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fxtec_msm8998
 
 # NFC
 BOARD_NFC_CHIPSET := pn553

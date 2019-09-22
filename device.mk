612f26f8c5f3a72918e47888befd1881c2ed1c75
Pranav Vashi <neobuddy89@gmail.com>
2019-09-22 13:31:47 +0530

    pro1: Create system copy of ims-ext-common and add to classpath
    Change-Id: Ib12c44e1d55aa5e2ff21af63b7fe281ca523bae3

--- device.mk
+++ device.mk
@@ -388,6 +388,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    ims-ext-common_system \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
@@ -396,6 +397,7 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
+    ims-ext-common_system \
     telephony-ext
 
 # Seccomp policy

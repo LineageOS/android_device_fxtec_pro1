c144cd585a4fa1bfa7b1d90ae98c90a4b2163e03
Ethan Chen <intervigil@gmail.com>
2020-03-01 16:58:14 -0800

    pro1: Simplify gpt-utils inclusions
    Change-Id: I43ace88d3cd43b7c86bc391ca787e9c67fd66067

--- gpt-utils/Android.mk
+++ gpt-utils/Android.mk
@@ -2,22 +2,22 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := bootable/recovery \
-		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE := libgptutils
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := bootable/recovery \
-		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_SHARED_LIBRARIES += liblog libcutils
+LOCAL_EXPORT_HEADER_LIBRARY_HEADERS := libgptutils_headers
 LOCAL_MODULE := libgptutils
-LOCAL_COPY_HEADERS_TO := gpt-utils/inc
-LOCAL_COPY_HEADERS := gpt-utils.h
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgptutils_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HEADER_LIBRARY)

ca19aad7c0269187bb0ec3f3012f2e3548db2a7b
Ethan Chen <intervigil@gmail.com>
2020-03-01 21:50:48 -0800

    pro1: Move init.qcom.rc to rootdir
    Change-Id: Ie9f5bee9bb90756af649a3231efecfdcf9420462

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -13,5 +13,6 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)

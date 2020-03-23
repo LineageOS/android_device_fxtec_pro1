b0cf2343836ba8d60b9550291b1993c2f51be0f2
Ethan Chen <intervigil@gmail.com>
2020-03-22 20:36:00 -0700

    pro1: Clean up init.btlfp.sh
    * Remove useless logging statement, init will track the service
      start/stop time in the log as well as in a system property.
    * Convert tabs to spaces to line up with the rest of the system.
    
    Change-Id: I9ce2e96a9c4e9c34d6432ede13edd6e83479bf6f

--- rootdir/etc/init.btlfp.sh
+++ rootdir/etc/init.btlfp.sh
@@ -4,18 +4,15 @@ fwfile="/data/system/users/0/settings_fingerprint.xml"
 hwdir="/persist/data/fingerprint"
 
 logmsg() {
-	/vendor/bin/log -t "fingerprint" "$@"
+    /vendor/bin/log -t "fingerprint" "$@"
 }
-
-
-logmsg "$0 starting"
 
 # If the framework fingerprint data does not exist, ensure that the
 # hardware data is also deleted.
 if [ ! -f "$fwfile" ]; then
-	logmsg "$fwfile does not exist"
-	if [ -d "$hwdir" ]; then
-		logmsg "$hwdir exists, deleting"
-		rm -rf "$hwdir"
-	fi
+    logmsg "$fwfile does not exist"
+    if [ -d "$hwdir" ]; then
+        logmsg "$hwdir exists, deleting"
+        rm -rf "$hwdir"
+    fi
 fi

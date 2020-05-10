8d1b71c2e295a59f5a5c4be6fc54b191e7d34539
Ethan Chen <intervigil@gmail.com>
2020-05-09 23:33:07 -0700

    pro1: Disable kernel console
    Change-Id: Ia210f67d6bfa1525439455b8c68926cd24cf9486

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,8 +50,7 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc1b0000 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_hmp=1
 BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 service_locator.enable=1

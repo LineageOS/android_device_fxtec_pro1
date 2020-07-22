a1f6998e0e0036507af8ba685ec55882b231e0c9
Bharat Pawar <bpawar@codeaurora.org>
2020-07-22 20:54:03 +0530

    pro1: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    
    Change-Id: I7529dc91c2ff994b83c7b9e7ddf4df913b9205f9

--- device.mk
+++ device.mk
@@ -440,11 +440,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.pro1
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
-
 # VNDK
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so

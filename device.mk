13097e289ffa47cae72aa990ce69deaab44193ba
Bruno Martins <bgcngm@gmail.com>
2020-10-21 21:08:54 +0100

    pro1: Remove incorrect PRODUCT_CHARACTERISTICS flag
    This target supports external sdcards. Gonna hate you more.
    
    Change-Id: I35a55c0bc5dbea7b8e859482ab46d9f7b1886ded

--- device.mk
+++ device.mk
@@ -111,9 +111,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Haters gonna hate..
-PRODUCT_CHARACTERISTICS := nosdcard
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \

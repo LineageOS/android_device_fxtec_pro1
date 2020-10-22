a8b6b3864b58c1e9338c9e51bea53b9205b6a562
Bruno Martins <bgcngm@gmail.com>
2020-10-22 20:21:03 +0100

    pro1: Switch to {odm,system,vendor}.prop
    AOSP now supports this and eases changes tracking.
    
    Change-Id: I7685e7381a61000fe9f2c89f21788516e7f42c00

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,11 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC_OVERLAY_TEST_EXT="$(abspath prebuilts/misc/linux-x86/libufdt/ufdt_apply_overlay)"
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 

a498d72050f1de9a2155413e74c5139f8757f2d7
Bruno Martins <bgcngm@gmail.com>
2020-11-08 21:19:44 +0000

    pro1: Switch to aidl power HAL
    Change-Id: I20bf3b1ef738a9c7dc94514536c3ea162e3a8468

--- device.mk
+++ device.mk
@@ -358,7 +358,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pro1
+    android.hardware.power-service.pro1-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -428,6 +428,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
     hardware/google/pixel \
     vendor/nxp/opensource/pn5xx
 

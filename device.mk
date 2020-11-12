ecd1976caa9afbed700c502e9385e09203e24332
Bruno Martins <bgcngm@gmail.com>
2020-11-12 17:55:14 +0000

    Revert "pro1: Create system copy of ims-ext-common and add to classpath"
    This reverts commit daff15660efeac39a95f59b3542ca813c0249942.
    
    The hack is no longer needed after updating to an ims.apk compatible
    with Android 10.
    
    Change-Id: I2bd91e8ee9f8e25bc40b017e9b1be3fb32fce900

--- device.mk
+++ device.mk
@@ -405,7 +405,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ims-ext-common \
-    ims-ext-common_system \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
@@ -414,7 +413,6 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system \
     telephony-ext
 
 # Seccomp policy

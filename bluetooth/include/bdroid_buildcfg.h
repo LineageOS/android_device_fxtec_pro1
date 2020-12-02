c23255cfab4596948360c0dba0945f65f7f094dd
Bruno Martins <bgcngm@gmail.com>
2020-12-02 21:43:48 +0000

    pro1: bluetooth: Disable extended QC features
    This is a no-op with pure AOSP BT stack. Moreover,
    it's enabled by default if devices opt-in for QTI BT stack.
    
    Change-Id: I0ad604d7a7a502df647676b1afc77d770fa50710

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -5,7 +5,7 @@
  *  for attribution purposes only.
  *
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017,2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME "FxTec Pro1"
-#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16

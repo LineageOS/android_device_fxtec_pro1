4db103e66ac9d96eab4800b1536dab923daa9096
Marc Bourgoin <themard85@gmail.com>
2021-02-10 22:38:58 -0700

    pro1: Add a check before adding dependencies
    Without a check, when performing a vendor blob regen it'll add another
    dependency on top of any existing one
    
    Change-Id: I9c7f17082fd850235c794d44cec3cda0c4b883ef

--- extract-files.sh
+++ extract-files.sh
@@ -45,7 +45,9 @@ function blob_fixup() {
             sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
             ;;
         system_ext/lib64/lib-imsvideocodec.so)
-            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            for LIBUI_SHIM in $(grep -L "libui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libui_shim.so" "$LIBUI_SHIM"
+            done
             ;;
         system_ext/lib64/libdpmframework.so)
             sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"

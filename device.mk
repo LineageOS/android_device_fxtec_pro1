d1f6d0a394105a1dba45f84bf8a4eb89aa0f01c6
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    pro1: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- device.mk
+++ device.mk
@@ -386,6 +386,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full
 
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \

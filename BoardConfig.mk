7c4d6530f135baa4d5c03d5f0fd644e2fac82f7b
Bruno Martins <bgcngm@gmail.com>
2022-02-11 23:56:06 +0000

    [DNM][TMP] pro1: Temp bringup
    Change-Id: Idef5bb1b642f891a99a6f7a6d7920ad0323b9339

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@
 #
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
 
 DEVICE_PATH := device/fxtec/pro1
 
@@ -56,6 +57,7 @@ BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb

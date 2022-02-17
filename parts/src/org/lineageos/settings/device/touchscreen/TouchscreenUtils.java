7f36c78b26aadac35a930bac1443a43877da437d
Bruno Martins <bgcngm@gmail.com>
2022-02-17 21:45:07 +0000

    pro1: Merge DeviceSettings and Doze into FxtecParts
    Change-Id: I67d33d6cc839f5ae43bba17963264d46e413d462

---
+++ parts/src/org/lineageos/settings/device/touchscreen/TouchscreenUtils.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device.touchscreen;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import org.lineageos.internal.util.FileUtils;
+import org.lineageos.settings.device.R;
+
+public class TouchscreenUtils {
+
+    private static final String TAG = TouchscreenUtils.class.getSimpleName();
+    private static final boolean DEBUG = false;
+
+    public static void setTouchscreenMargin(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        final int margin = Constants.TOUCHSCREEN_MARGIN_STEP *
+                prefs.getInt(Constants.TOUCHSCREEN_MARGIN_KEY,
+                        context.getResources().getInteger(R.integer.touchscreen_margin_default));
+        FileUtils.writeLine(Constants.TOUCHSCREEN_MARGIN_SYS_FILE,
+                Integer.toString(margin));
+    }
+}

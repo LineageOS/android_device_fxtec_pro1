25f57f625b26017f7ced82effe75b71b805523b0
Sean McCreary <mccreary@mcwest.org>
2022-03-10 17:12:30 -0700

    pro1: overlay: Allow launcher home screen rotation
     * Added as overlay package since RRO is not auto generating.
    
    This implements the workaround for issue
    https://gitlab.com/LineageOS/issues/android/-/issues/3873
    
    Co-authored-by: Alexander Koskovich <akoskovich@pm.me>
    Change-Id: Ib43a8520650c7e67f3c62fe946622fa49aa7cfc9

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+PRODUCT_PACKAGES += \
+    TrebuchetDeviceOverlay
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi

ef728be829217055ea483545cc1ada370c5fbaee
Bruno Martins <bgcngm@gmail.com>
2022-03-22 19:38:08 +0000

    pro1: devicesettings: Follow SuW's new button theming
    Change-Id: I61dd51aeeb111fc43efea1cb01be8efab9e773ca

--- devicesettings/src/org/lineageos/settings/device/setupwizard/SetupWizardBaseActivity.java
+++ devicesettings/src/org/lineageos/settings/device/setupwizard/SetupWizardBaseActivity.java
@@ -17,6 +17,7 @@
 package org.lineageos.settings.device.setupwizard;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -25,6 +26,7 @@ import android.widget.Button;
 
 import com.android.settingslib.Utils;
 
+import com.google.android.setupcompat.template.FooterButtonStyleUtils;
 import com.google.android.setupdesign.GlifLayout;
 
 import org.lineageos.settings.device.R;
@@ -56,19 +58,24 @@ public class SetupWizardBaseActivity extends Activity implements View.OnClickLis
         if (getLayoutResId() != -1) {
             setContentView(getLayoutResId());
         }
+
+        final GlifLayout layout = getGlifLayout();
+        final Context context = layout.getContext();
+
         if (getTitleResId() != -1) {
             final CharSequence headerText = TextUtils.expandTemplate(getText(getTitleResId()));
-            getGlifLayout().setHeaderText(headerText);
+            layout.setHeaderText(headerText);
         }
         if (getIconResId() != -1) {
-            final GlifLayout layout = getGlifLayout();
             final Drawable icon = getDrawable(getIconResId()).mutate();
-            icon.setTintList(Utils.getColorAccent(layout.getContext()));
+            icon.setTintList(Utils.getColorAccent(context));
             layout.setIcon(icon);
         }
 
         Button buttonNext = findViewById(R.id.btn_next);
         buttonNext.setOnClickListener(this);
+
+        FooterButtonStyleUtils.applySecondaryButtonPartnerResource(context, buttonNext, true);
 
         setupPage();
     }

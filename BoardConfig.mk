f05d2d0cac87d7ace9a221dd748444e00d327ed0
Bruno Martins <bgcngm@gmail.com>
2022-09-08 12:55:56 +0100

    pro1: Remove extraneous comments from makefiles
    Fixup the project naming in the copyrights while at it.
    
    Change-Id: Ic8c092aa87f0c69a90fd28b14ebe21f34a9806da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017,2020-2021 The LineageOS Open Source Project
+# Copyright (C) 2017,2020-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
 #
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true

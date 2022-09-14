c14a3e3627362efc5c1aed627db9f6fb6f73a50b
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    pro1: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fastbootd
 PRODUCT_PACKAGES += \

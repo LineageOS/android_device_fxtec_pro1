ee1c36be2b04cdc4a8ccc178a65ba2a93a60c094
me-cafebabe <me.cafebabe@gmail.com>
2023-10-07 17:26:01 +0800

    pro1: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7

--- device.mk
+++ device.mk
@@ -481,7 +481,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     hostapd_cli \
     libwifi-hal-qcom \

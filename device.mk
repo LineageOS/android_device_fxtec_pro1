2369bce89e8d06e287b10743d0452267ee5e741b
LuK1337 <priv.luk@gmail.com>
2023-10-21 15:44:06 +0200

    pro1: Build android.hardware.bluetooth@1.0
    Required by libantradio.
    
    Change-Id: I5e26a4154cf54e64f424cb6cf20db87b7023f1cd

--- device.mk
+++ device.mk
@@ -145,6 +145,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default

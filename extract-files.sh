2caa0e4cc1f2e4569dee9c2a96fe6a8871d6a15e
Bruno Martins <bgcngm@gmail.com>
2024-03-30 14:11:54 +0000

    pro1: Bringup telephony after U QPR2
    Even though a small part of this mostly seems like a revert of commit
    1a96855, the fact is that IMS really works now and telephony is up after
    the big Android U QPR2 merge.
    
    Notably, pd-mapper service must be kept for cnss power voting.
    
    Change-Id: Id272a09338b285d419f8f012cfc6b62a7f8ef50c

--- extract-files.sh
+++ extract-files.sh
@@ -29,20 +29,7 @@ function blob_fixup() {
         system/lib64/com.qualcomm.qti.ant@1.0.so|system/lib64/vendor.qti.hardware.fm@1.0.so)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
-        system_ext/etc/init/dpmd.rc)
-            sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml)
-            ;&
-        system_ext/etc/permissions/dpmapi.xml)
-            sed -i "s/\/system\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/embms.xml)
-            ;&
-        system_ext/etc/permissions/qcrilhook.xml)
-            sed -i "s/\/system\/framework\//\/system\/system_ext\/framework\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/qti_libpermissions.xml)
+        system_ext/etc/permissions/com.qti.dpmframework.xml|system_ext/etc/permissions/qti_libpermissions.xml)
             sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
             ;;
         system_ext/lib64/lib-imscamera.so)
@@ -51,18 +38,10 @@ function blob_fixup() {
         system_ext/lib64/lib-imsvideocodec.so)
             grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            "${PATCHELF}" --replace-needed "libqdMetaData.so" "libqdMetaData.system.so" "${2}"
             ;;
-        system_ext/lib64/lib-imsvt.so)
+        vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti|vendor/bin/hw/btlfpserver|vendor/bin/hw/vendor.display.color@1.0-service|vendor/bin/hw/vendor.qti.esepowermanager@1.0-service|vendor/bin/hw/vendor.qti.gnss@1.0-service|vendor/bin/hw/vendor.qti.hardware.qdutils_disp@1.0-service-qti|vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service|vendor/bin/hw/vendor.qti.hardware.soter@1.0-service|vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-            ;;
-        system_ext/lib64/libdpmframework.so)
-            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
-            ;;
-        vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti|vendor/bin/hw/btlfpserver|vendor/bin/hw/vendor.display.color@1.0-service|vendor/bin/hw/vendor.qti.esepowermanager@1.0-service|vendor/bin/hw/vendor.qti.gnss@1.0-service|vendor/bin/hw/vendor.qti.hardware.qdutils_disp@1.0-service-qti|vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service|vendor/bin/hw/vendor.qti.hardware.soter@1.0-service|vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti|vendor/bin/ATFWD-daemon|vendor/bin/cnd|vendor/bin/ims_rtp_daemon|vendor/bin/imsrcsd|vendor/bin/netmgrd)
-            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-            ;;
-        vendor/bin/pm-service)
-            grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
             ;;
         vendor/lib/hw/camera.msm8998.so)
             sed -i "s/service.bootanim.exit/service.bootanim.zzzz/g" "${2}"
@@ -73,7 +52,7 @@ function blob_fixup() {
         vendor/lib64/hw/fingerprint.msm8998.so)
             sed -i "s/libhidltransport.so/libhidlbase_shim.so/" "${2}"
             ;;
-        vendor/lib64/libcne.so|vendor/lib64/libcneapiclient.so|vendor/lib64/libril-qc-qmi-1.so|vendor/lib64/libsecureui.so)
+        vendor/lib64/libsecureui.so)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
     esac

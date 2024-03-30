b269473fff2f83e92f21d0fc502db7c5959aa1db
Bruno Martins <bgcngm@gmail.com>
2024-03-30 14:11:54 +0000

    pro1: Bringup telephony after U QPR2
    Change-Id: Id272a09338b285d419f8f012cfc6b62a7f8ef50c

--- device.mk
+++ device.mk
@@ -322,6 +322,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+PRODUCT_PACKAGES += \
+    libavservices_minijail.vendor
+
 # Netd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
@@ -387,10 +390,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.1.vendor \
-    android.hardware.radio.config@1.0.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     librmnetctl \
     libxml2
 
--- extract-files.sh
+++ extract-files.sh
@@ -35,9 +35,6 @@ function blob_fixup() {
         system_ext/etc/permissions/com.qti.dpmframework.xml)
             ;&
         system_ext/etc/permissions/dpmapi.xml)
-            sed -i "s/\/system\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/embms.xml)
             ;&
         system_ext/etc/permissions/qcrilhook.xml)
             sed -i "s/\/system\/framework\//\/system\/system_ext\/framework\//g" "${2}"
@@ -58,11 +55,8 @@ function blob_fixup() {
         system_ext/lib64/libdpmframework.so)
             sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
-        vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti|vendor/bin/hw/btlfpserver|vendor/bin/hw/vendor.display.color@1.0-service|vendor/bin/hw/vendor.qti.esepowermanager@1.0-service|vendor/bin/hw/vendor.qti.gnss@1.0-service|vendor/bin/hw/vendor.qti.hardware.qdutils_disp@1.0-service-qti|vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service|vendor/bin/hw/vendor.qti.hardware.soter@1.0-service|vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti|vendor/bin/ATFWD-daemon|vendor/bin/cnd|vendor/bin/ims_rtp_daemon|vendor/bin/imsrcsd|vendor/bin/netmgrd)
+        vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti|vendor/bin/hw/btlfpserver|vendor/bin/hw/vendor.display.color@1.0-service|vendor/bin/hw/vendor.qti.esepowermanager@1.0-service|vendor/bin/hw/vendor.qti.gnss@1.0-service|vendor/bin/hw/vendor.qti.hardware.qdutils_disp@1.0-service-qti|vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service|vendor/bin/hw/vendor.qti.hardware.soter@1.0-service|vendor/bin/hw/vendor.qti.hardware.tui_comm@1.0-service-qti)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-            ;;
-        vendor/bin/pm-service)
-            grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
             ;;
         vendor/lib/hw/camera.msm8998.so)
             sed -i "s/service.bootanim.exit/service.bootanim.zzzz/g" "${2}"
@@ -73,7 +67,7 @@ function blob_fixup() {
         vendor/lib64/hw/fingerprint.msm8998.so)
             sed -i "s/libhidltransport.so/libhidlbase_shim.so/" "${2}"
             ;;
-        vendor/lib64/libcne.so|vendor/lib64/libcneapiclient.so|vendor/lib64/libril-qc-qmi-1.so|vendor/lib64/libsecureui.so)
+        vendor/lib64/libsecureui.so)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
     esac

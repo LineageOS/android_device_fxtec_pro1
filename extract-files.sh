0d4b1964d6e0d91d413339a51f1dcb45a6117864
Bruno Martins <bgcngm@gmail.com>
2024-03-30 14:11:54 +0000

    pro1: Bringup telephony after U QPR2
    Change-Id: Id272a09338b285d419f8f012cfc6b62a7f8ef50c

--- extract-files.sh
+++ extract-files.sh
@@ -35,9 +35,6 @@ function blob_fixup() {
         system_ext/etc/permissions/com.qti.dpmframework.xml)
             ;&
         system_ext/etc/permissions/dpmapi.xml)
-            sed -i "s/\/system\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
-            ;;
-        system_ext/etc/permissions/embms.xml)
             ;&
         system_ext/etc/permissions/qcrilhook.xml)
             sed -i "s/\/system\/framework\//\/system\/system_ext\/framework\//g" "${2}"

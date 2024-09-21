a54088ba96554b3449d31ffa542ac7326b0c0ccf
Bruno Martins <bgcngm@gmail.com>
2024-09-21 15:59:56 +0100

    pro1: Build missing camera dependency
    Change-Id: If70d417eb39c6ca24ba382cfddebbeb7ba4fa50b

--- device.mk
+++ device.mk
@@ -169,6 +169,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6:64 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    libion.vendor:32 \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Configstore

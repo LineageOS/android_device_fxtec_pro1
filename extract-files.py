e5c3b700bcd259a806853d1ddee96512f1233539
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-29 21:29:08 -0500

    pro1: Fix ELF checks on FP3 kangs
    Change-Id: I7e643a7370fa1d79cca58e4a03a6f93d03d5fd3a

--- extract-files.py
+++ extract-files.py
@@ -38,6 +38,7 @@ lib_fixups: lib_fixups_user_type = {
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccvndhal@1.0',
     ): lib_fixup_vendor_suffix,
     (
         'libmm-omxcore',

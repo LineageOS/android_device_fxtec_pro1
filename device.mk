e25c1522698a17b99af32fd8ab5ea04e0b3c7556
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-12 14:56:47 -0500

    pro1: Don't build android.hardware.power@1.2.vendor
    * Not a dep in vendor, not a dep of anything?
    
    Change-Id: I94ff4cc5b6af60ea5e4545e8d8af73daa26343e4

--- device.mk
+++ device.mk
@@ -315,8 +315,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    android.hardware.power@1.2.vendor
+    android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json

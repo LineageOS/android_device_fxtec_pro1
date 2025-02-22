47f08723900c088a4e95cc84ad1eb509dc50bc08
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-22 02:21:03 -0500

    pro1: Migrate bluetooth firmware symlinks to BP
    Change-Id: I1e1bf16b97f08445bfe10afe38a971c31ca03978

--- Android.mk
+++ Android.mk
@@ -22,14 +22,4 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-BT_FIRMWARE := apbtfw10.tlv apnv10.bin crbtfw11.tlv crbtfw20.tlv crbtfw21.tlv crnv11.bin crnv20.bin crnv21.bin
-BT_FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(BT_FIRMWARE)))
-$(BT_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating BT firmware symlink: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/bt_firmware/image/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(BT_FIRMWARE_SYMLINKS)
-
 endif

cc391c6f05aebc1718d9acc6b5040917ab274e1a
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    pro1: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- device.mk
+++ device.mk
@@ -394,6 +394,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
 
+# Vendor init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_fxtec_msm8998)
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service

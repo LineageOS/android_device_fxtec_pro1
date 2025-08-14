1d85669d11e8201fb95e2bc895ea8b38d8363bfe
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    pro1: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- device.mk
+++ device.mk
@@ -285,7 +285,9 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
 
 # Lineage Health
 PRODUCT_PACKAGES += \
